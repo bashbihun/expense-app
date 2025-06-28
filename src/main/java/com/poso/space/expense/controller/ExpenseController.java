47f71a455a00fe009594ee2479e8592fe5592b71
bashbihun <bashbihun@gmail.com>
2025-06-28 20:11:12 +0700

    menambah fitur get by id

--- src/main/java/com/poso/space/expense/controller/ExpenseController.java
+++ src/main/java/com/poso/space/expense/controller/ExpenseController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,5 +35,11 @@ public class ExpenseController {
     public ResponseEntity<List<Expense>> getAllExpense() {
         List<Expense> listExpenses = expenseService.getAllExpense();
         return ResponseEntity.ok(listExpenses);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Expense> getExpenseById(@PathVariable Long id) {
+        Expense expense = expenseService.getExpenseById(id);
+        return ResponseEntity.ok(expense);
     }
 }
--- src/main/java/com/poso/space/expense/service/ExpenseService.java
+++ src/main/java/com/poso/space/expense/service/ExpenseService.java
@@ -3,7 +3,9 @@ package com.poso.space.expense.service;
 import java.util.List;
 
 import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.poso.space.expense.dto.ExpenseDTO;
 import com.poso.space.expense.entity.Expense;
@@ -25,5 +27,10 @@ public class ExpenseService {
 
     public List<Expense> getAllExpense() {
         return expenseRepository.findAll();
+    }
+
+    public Expense getExpenseById(Long id) {
+        return expenseRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "not found"));
     }
 }
